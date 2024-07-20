6cd36e77fb2fde24d380dec41dcfbb49f9be7f26
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-07-20 19:26:04 +0300

    add socket and parser

---
+++ cppAPP/src/clientHandler/clientHandler.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h> 
+
+namespace sh {
+
+class clientHandler
+{
+
+};
+
+}
